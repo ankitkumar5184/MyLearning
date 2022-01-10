68b25288ffdba628f567c4bd3761bcc14350a529
AnkitKumar <ankit.kumar@infostride.com>
2022-01-10 20:28:29 +0530

    Multidimenstional array program

---
+++ Array/Multidimenstional.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class Multidimenstional {
+    public static void main(String[] args)
+    {
+
+        int[][] arr = { { 1, 2 ,3}, { 3, 4,4} };
+        System.out.print("arr[][]= "+ arr[1][0]);
+    }
+
+}
