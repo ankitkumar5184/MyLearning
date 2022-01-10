9e96b273ce592f182c51458c1fb30e0dfad87040
AnkitKumar <ankit.kumar@infostride.com>
2022-01-10 14:23:25 +0530

    Dynamic array program.

---
+++ Array/Array1.java
@@ -0,0 +1,9 @@
+package Array;
+
+public class Array1 {
+    public static void main(String args[])
+    {
+        int a[]={10,20,30,40,50,70,532,56}; // simple array program
+        System.out.print(a[3]);
+    }
+}
---
+++ Array/Array2.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class Array2 {
+    public static void main(String args[])
+    {
+        int a[] =new int[5];
+        a[0]=10;a[2]=20;a[3]=45;a[4]=63; //STATIC ARRAY PROGRAM
+        for(int i=0;i<4;i++)
+        {
+            System.out.println(a[i]);
+        }
+    }
+}
