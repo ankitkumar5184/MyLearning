355a7c3cdbe482e43b83b24c98233059ff4ab781
AnkitKumar <ankit.kumar@infostride.com>
2022-01-11 17:26:10 +0530

    Replace the string "d" with "f".

---
+++ Replace.java
@@ -0,0 +1,8 @@
+public class Replace {
+    public static void main(String[] args){
+        String str = "The quick brown fox jumps over the lazy dog.";
+        String rs = str.replace("d","f");
+        System.out.println(rs);
+
+    }
+}
